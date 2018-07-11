0ba90678324bbd5a789da5dfe10e8f8bf13657be
BogieBogard <mbogard@utexas.edu>
2018-07-11 15:34:08 -0500

    Minor improvements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // Homework Steps
-// 1. Transfer the animal buttons into an array that the below for loop can use
-// 2. Create additional animal buttons
+// 1. Transfer the marvel buttons into an array that the below for loop can use
+// 2. Create additional marvel buttons
 // 3. Provide a text bar that the user can use to add a custom item to the array and thus creates a new button
 // 4. Add pause/play functionality to each gif result
 
@@ -16,7 +16,7 @@ for (var i = 0; i < marvelArray.length; i++) {
 $("button").on("click", function (event) {
   var character = $(this).attr("character-name");
 
-  // Constructing a queryURL using the animal name
+  // Constructing a queryURL using the marvel name
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     character + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -39,17 +39,14 @@ $("button").on("click", function (event) {
         // Creating and storing a div tag
         var marvelDiv = $("<div>");
         console.log(results[i]);
-        // Creating a paragraph tag with the result item's rating
-        var p = $("<p>").text("Rating: " + results[i].rating);
 
         // Creating and storing an image tag
-        var animalImage = $("<img>");
+        var marvelImage = $("<img>");
         // Setting the src attribute of the image to a property pulled off the result item
-        animalImage.attr("src", results[i].images.fixed_height.url);
+        marvelImage.attr("src", results[i].images.fixed_height.url);
 
-        // Appending the paragraph and image tag to the marvelDiv
-        marvelDiv.append(p);
-        marvelDiv.append(animalImage);
+        // Appending the image tag to the marvelDiv
+        marvelDiv.append(marvelImage);
 
         // Prependng the marvelDiv to the HTML page in the "#gifs-appear-here" div
         $("#gifs-appear-here").prepend(marvelDiv);
@@ -78,7 +75,7 @@ function customButtonFunction() {
   $("button").on("click", function (event) {
     var character = $(this).attr("character-name");
   
-    // Constructing a queryURL using the animal name
+    // Constructing a queryURL using the marvel name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       character + "&api_key=dc6zaTOxFJmzC&limit=10";
   
@@ -101,17 +98,14 @@ function customButtonFunction() {
           // Creating and storing a div tag
           var marvelDiv = $("<div>");
           console.log(results[i]);
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
   
           // Creating and storing an image tag
-          var animalImage = $("<img>");
+          var marvelImage = $("<img>");
           // Setting the src attribute of the image to a property pulled off the result item
-          animalImage.attr("src", results[i].images.fixed_height.url);
+          marvelImage.attr("src", results[i].images.fixed_height.url);
   
-          // Appending the paragraph and image tag to the marvelDiv
-          marvelDiv.append(p);
-          marvelDiv.append(animalImage);
+          // Appending the image tag to the marvelDiv
+          marvelDiv.append(marvelImage);
   
           // Prependng the marvelDiv to the HTML page in the "#gifs-appear-here" div
           $("#gifs-appear-here").prepend(marvelDiv);
